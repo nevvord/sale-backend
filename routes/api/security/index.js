9ad1f20939668df9fa19640a81bc3f0a13473f3a
Nevvord <nevvord@gmail.com>
2019-11-10 19:53:15 +0300

    hf

--- routes/api/security/index.js
+++ routes/api/security/index.js
@@ -2,19 +2,7 @@ const { ip } = require('config')
 
 module.exports = (req, res, next) => {
     let auth = false
-    ip.forEach(element => {
-        if(element === req.connection.remoteAddress){
-            auth = true
-        }
-    })
-
-    if(auth){
-        next()
-    }else{
-        res.status(403).send({
-            msg: "Ваш IP отсутствует в допуске."
-        })
-    }
-    
-    
+    auth = ip.forEach(element => element === req.connection.remoteAddress)
+    next()
+    //auth === true ? next() : res.status(403).send({msg: "Ваш IP отсутствует в допуске."})
 }
